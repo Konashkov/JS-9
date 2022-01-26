1c810f47e4e23ec393cd3389248e1d24f36c14ef
Konashkov <79665788+Konashkov@users.noreply.github.com>
2022-01-26 19:33:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let thisArray = [10, 2 ,45, 78, 56, 2 ,10]
+let number2 = Number(prompt("Enter number, what you are looking for"))
+let findedNumbers = [];
+for(let a = 0;a < thisArray.length;a++){
+ if(thisArray[a]>=number2) findedNumbers.push(thisarray[a])
+}
+console.log(findedIndexes)
